37b4081dc047b9dbabaa74776108dbede5ba445d
Sergey Kolesnik <sergey.kolesnik.ld@gmail.com>
2023-04-07 22:08:44 +0300

    #1 added initial functional pytest suite

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+﻿import ipaddress
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--server_ip",
+        action="store",
+        help="IP address of the server to test",
+    )
+    parser.addoption(
+        "--server_port",
+        action="store",
+        type=int,
+        default=23,
+        help="Port of the server to test",
+    )
+    parser.addoption(
+        "--lines_count",
+        action="store",
+        type=int,
+        default=1000,
+        help="Number of lines to process",
+    )
+
+
+@pytest.fixture
+def server_ip(request):
+    return ipaddress.IPv4Address(request.config.getoption("--server_ip"))
+
+
+@pytest.fixture
+def server_port(request):
+    return request.config.getoption("--server_port")
+
+
+@pytest.fixture
+def lines_count(request):
+    return request.config.getoption("--lines_count")
---
+++ tests/test_client_requests.py
@@ -0,0 +1,5 @@
+import ipaddress
+
+
+def test_n_lines(server_ip: ipaddress.IPv4Address, server_port: int, lines_count: int = 1000):
+    assert False, "this test is not implemented"
