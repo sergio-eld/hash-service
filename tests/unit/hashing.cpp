ffa2069b4698ed2776dd22fc27d12ebe62f3635b
Sergey Kolesnik <sergey.kolesnik.ld@gmail.com>
2023-04-09 18:57:54 +0300

    #1 added hashing unit tests (not implemented). Implemented hashing class (sha256). Modified project structure.

---
+++ tests/unit/hashing.cpp
@@ -0,0 +1,32 @@
+
+#include "hash-service/hash.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+	TEST(Hashing, Line) {
+		// TODO: implement
+		FAIL();
+	}
+
+	TEST(Hashing, LoremIpsum) {
+		// TODO: implement
+		FAIL();
+	}
+
+	TEST(Hashing, LoremIpsumInChunks){
+		// TODO: implement
+		FAIL();
+	}
+
+	TEST(Hashing, Multithreaded) {
+		// TODO: implement
+		FAIL();
+	}
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
